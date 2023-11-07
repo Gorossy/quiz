9b446bec406ac407ebf47eb757c0dd19e919aa7b
Gorossy <gorossy07@gmail.com>
2023-11-06 22:31:54 -0500

    fix: five last exams

--- quizengine/views.py
+++ quizengine/views.py
@@ -1,6 +1,6 @@
 from rest_framework.response import Response
 from rest_framework import status, generics
-from .models import Exam, UserStatistics
+from .models import Exam, UserStatistics, UserExam
 from .serializers import ExamSerializer, ExamListSerializer, UserExamSerializer,UserRegistrationSerializer
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.views import APIView
@@ -37,11 +37,22 @@ class UserStatisticsView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request, format=None):
+        user = request.user
         try:
-            user_statistics = UserStatistics.objects.get(user=request.user)
+            user_statistics = UserStatistics.objects.get(user=user)
+            last_five_exams = UserExam.objects.filter(user=user).order_by('-completed_at')[:5]
+            last_five_exams_data = [
+                {
+                    "id": user_exam.exam.id,
+                    "exam_title": user_exam.exam.title,
+                    "score": user_exam.score
+                } for user_exam in last_five_exams
+            ]
+            
             return Response({
                 "average_score": user_statistics.average_score,
                 "quizzes_completed": user_statistics.quizzes_completed,
+                "last_five_exams": last_five_exams_data
             })
         except UserStatistics.DoesNotExist:
             return Response({"error": "Statistics do not exist for this user."}, status=404)
